e0aa46a8ae00095b7f037b919c02f99ddaa5ba84
ObedGreatness <obedyawson155@gmail.com>
2024-03-26 19:20:47 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,29 @@
-#!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
+import requests
+import sys
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
-  }
-});
+def get_movie_characters(movie_id):
+    url = f"https://swapi.dev/api/films/{movie_id}/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        movie_data = response.json()
+        characters_urls = movie_data['characters']
+        characters_names = []
+        for char_url in characters_urls:
+            char_data = requests.get(char_url).json()
+            characters_names.append(char_data['name'])
+        return characters_names
+    else:
+        print("Failed to retrieve movie data. Please check your input.")
+        return None
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <movie_id>")
+        sys.exit(1)
+    
+    movie_id = sys.argv[1]
+    characters = get_movie_characters(movie_id)
+    if characters:
+        for character in characters:
+            print(character)
+
